e38024b8222c529ce04e7af576fa9e48167d3741
Joseph L <josephlei@users.noreply.github.com>
2015-06-23 01:14:21 -0700

    modified spider to pull text

--- cajobs/spiders/alljobs.py
+++ cajobs/spiders/alljobs.py
@@ -11,3 +11,33 @@ class AlljobsSpider(scrapy.Spider):
 
     def parse(self, response):
         filename=response.url.split("/")[-2]
+        #print filename
+        #x=response.xpath("//table")[1].xpath("./tr")
+
+        for sel in response.xpath("//table")[1].xpath("./tr"):
+        	#print sel.xpath("./td/text()")
+        	#job_title=sel.xpath("./td/text()")[0].extract()
+        	#job_exams=sel.xpath("./td/text()")[1].extract()
+        	#job_vacancies=sel.xpath("./td/text()")[2].extract()
+        	
+        	job_title=sel.xpath("./td/text()")[0].extract()
+        	
+        	try:
+        		job_exams=sel.xpath("./td/a/text()")[0].extract()
+        		job_vacancies=sel.xpath("./td/a/text()")[1].extract()
+        	except:
+        		pass
+        	#job_vacancies=sel.xpath("./td/text()")[2].extract()
+
+        	#print job_title, job_exams
+        	#print sel.xpath("./td/a/@href").extract() #this works!
+        	#print type(sel.xpath("./td/a/text()").extract())
+        	#print sel.xpath("./td/a/text()").extract()
+
+        	try:
+        		print job_exams,job_vacancies
+        	except:
+        		pass
+        	#print type(sel.xpath("./td")[0]) #iam selector
+        	#print job_exams
+        	#print job_vacancies
